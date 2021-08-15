3e4f9cde5b7e82c6422909b416bed1c90eb05ad7
Pradip Gharge <59740723+pradipgharge@users.noreply.github.com>
2021-08-16 03:43:36 +0530

    feat: eventlistener, function clickhandler

--- App.js
+++ App.js
@@ -2,3 +2,21 @@ var txtInput = document.querySelector("#input-text");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output-text");
 
+btnTranslate.addEventListener("click", clickHandler);
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => {
+                return response.json();
+            }
+
+        )
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+
+}
